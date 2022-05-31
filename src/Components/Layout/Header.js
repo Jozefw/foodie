44428c991e8bf35411ec3f41c30f9f0f8d7a9723
Jozef Woroniecki <jozefw@sbcglobal.net>
2022-05-31 22:40:12 +0000

    add git ignore and header file

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React, {fragment} from 'react';
+import bkgImage from '../../assets/bkg.jpg';
+
+ const Header = (props) =>{
+  return (
+
+    <>
+    <header>
+    <h1>Meals To Go</h1>
+    <button>Cart</button>
+    </header>
+    <div>
+        <img></img>
+    </div>
+    </>
+    
+  )
+}
+export default Header;
+
