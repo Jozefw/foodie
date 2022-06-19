a02d0533d942d174ad45f1dd54a01ee6e003e8b9
Jozef Woroniecki <jozefw@sbcglobal.net>
2022-06-19 23:16:57 +0000

    added cart button

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import CartIcon from '../Cart/CartIcon';
 
 const HeaderCartButton = ()=> {
   return (
     <button>
-      HeaderCartButton
-      </button>
+    <span><CartIcon>Cart</CartIcon></span>  
+    <span>Your Cart</span>  
+    <span>3</span>  
+    </button>
   )
 }
 export default HeaderCartButton;
