360a3c31bbfaa369c2e6dac573f06d3f0c354e2c
Jozef Woroniecki <jozefw@sbcglobal.net>
2022-07-01 23:24:07 +0000

    added meals n such

--- src/Components/Deserts/DesertItem/DesertItem.js
+++ src/Components/Deserts/DesertItem/DesertItem.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+import styles from './zDesertItem.module.css';
 
-export default function DesertItem() {
+export default function DesertItem(props) {
+
+  const price = `$${props.price.toFixed(2)}`
+
   return (
-    <div>DesertItem</div>
+    <li className={styles.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={styles.description}>{props.descr}</div>
+        <div className={styles.price}>{price}</div>
+      </div>
+     
+      <div></div>
+    </li>
   )
 }
