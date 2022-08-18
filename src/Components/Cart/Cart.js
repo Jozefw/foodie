02375bb054c1ff2f8ecb1cc2071e0813a25387d3
Jozef <Jozefw@sbcglobal.net>
2022-08-18 16:04:11 -0700

    add speling corection

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import stytles from '.Cart.modules.css';
+import styles from '.Cart.modules.css';
 
 const Cart =(props) =>{
-const cartitems =<ul className={styles['cart-items']}>{[{id:'c1',name:'One Pounder',price:12.99,amount:2}].map((item)=><li>item.name</li>)}</ul>
+const cartItems =<ul className={styles['cart-items']}>{[{id:'c1',name:'One Pounder',price:12.99,amount:2}].map((item)=><li>item.name</li>)}</ul>
 
   return (
     <div>
-      {cartitems}
+      {cartItems}
       <div></div>
       <div></div>
     </div>
