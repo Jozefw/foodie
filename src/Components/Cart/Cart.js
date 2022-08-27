79bcccf9c1072d9b892360dbc261bb219f9995b4
Jozef <Jozefw@sbcglobal.net>
2022-08-27 07:42:28 -0700

    add modal style fixes

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,20 +3,20 @@ import Modal from '../UI//Modal';
 import styles from './Cart.module.css';
 
 const Cart =(props) =>{
-const cartItems = <ul className={styles['cart-items']}>{[{id:'c1',name:'One Pounder',price:12.99,amount:2}].map((item)=><li key={item.id}>item.name</li>)}</ul>
+const cartItems = <ul className={styles['cart-items']}>{[{id:'c1',name:'One Pounder',price:12.99,amount:2}].map((item)=><li key={item.id}>{item.name}</li>)}</ul>
 
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={styles.total}>
       <span>Total Amount</span>
       <span>$62.54</span>
       </div>
-      <div className={styles.action}>
+      <div className={styles.actions}>
         <button className={styles['button--alt']}>Close</button>
         <button className={styles.button}>Order</button>
       </div>
-      </div>
+      </Modal>
   )
 }
 
