1dea12f3dceed7ebe64db4d98b6ee9b32a2c3fef
Jozef <Jozefw@sbcglobal.net>
2022-10-17 16:58:29 -0700

    adding context

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{useState} from 'react';
 import Header from './Components/Layout/Header';
 import Cart from './Components/Cart/Cart';
 import Deserts from './Components/Deserts/Deserts';
+import CartProvider from './CartProvider';
 
 
 function App() {
@@ -15,14 +16,14 @@ const hideCartHandler = () =>{
 }
 
   return (
-    <>
+    <CartProvider>
     {cartShown && <Cart onClose={hideCartHandler}></Cart> }
       <Header onShowingCart={showCartHandler}> </Header>
         <main>
         <Deserts></Deserts>
         </main>
      
-    </>
+    </CartProvider>
   );
 }
 
--- src/Store/CartProvider.js
+++ src/Store/CartProvider.js
@@ -1,5 +1,24 @@
+import CartContext from './CartContext';
+
 CartProvider = (props) =>{
 
+    const AddCartItemHandler = (item) =>{
+        
+    }
+    const RemoveCartItemHandler = (id) =>{
+
+    }
+
+    const AllCartContexts = {
+        items:[],
+        totalAmount:0,
+        addItem: AddCartItemHandler,
+        removeItem:RemoveCartItemHandler 
+    }
+
+return <CartContext value={AllCartContexts}>
+    {props.children}
+</CartContext>
 }
 
 export default CartProvider;
